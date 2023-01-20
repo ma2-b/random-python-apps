0927528c2f865c4d340762cb3987098195bbcfb9
ma2-b <mohammadabdelqaderbusiness298@gmail.com>
2023-01-20 16:02:37 +0200

    bot

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ massage = input("Enter the massage: ")
 
 num = int(input("Enter the number of massages you want to send: "))
 
-time.sleep(4)
+time.sleep(3)     
+
 for i in range(num):
     pg.write(massage)
     pg.press('enter')
